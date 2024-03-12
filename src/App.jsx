af1577f8fe3b63af36c450399da8863c50706bfd
Joseph Enubiak <enubiakjoseph@gmail.com>
2024-03-12 02:52:59 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import Header from "../Components/Header/Header";
 import Hero from "../Components/Hero/Hero";
-import './App.css'
+import "./App.css";
 import Residencies from "../Components/Residencies/Residencies";
+import Blog from "../Components/Blog/Blog";
 import Value from "../Components/Value/Value";
 import Contact from "../Components/Contact/Contact";
 import GetStarted from "../Components/GetStarted/GetStarted";
 import Footer from "../Components/Footer/Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
- 
-      <div className="App">
+    <div className="App">
       <div>
         <div className="white-gradient" />
-      <Header/>
-      <Hero />
+        <Header />
+        <Hero />
       </div>
-   <Residencies/>
-   <Value/>
-   <GetStarted/>
-   <Footer/>
+      <Routes>
+        <Route path="residencies/:id" element={<Blog />} />
+      </Routes>
+      <Residencies/>
+      <Value />
+      <GetStarted />
+      <Footer />
     </div>
-   
-    
   );
 }
 
