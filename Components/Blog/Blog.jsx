ebe2d2e72e45e6ee3856f44dbac844a18780a340
Richard Friday <richemonday@gmail.com>
2024-03-13 00:38:14 +0100

    restructure

--- Components/Blog/Blog.jsx
+++ Components/Blog/Blog.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React from 'react';
+import Header from '../Header/Header';
 
 const Blog = () => {
   return (
-    <div>
-      richard
-    </div>
+      <div className="flexCenter b-wrapper">
+        <div className="flexCenter B-header">
+        </div>
+        <div className="flexCenter b-container">
+          <span className="Blog-p">
+            <p>
+              jhgaxjhagsdjgasdjsduaskjdaskjd
+              gggggggggggggggggggggggggggggg
+              ggggggggggggggggggggggggggggggggdrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+              ggggggggggggggggggggggggggggggggggdffffffffffffffffffffffffffffff
+              fffffffffffffffffffffffff
+              jjjjjjjjjjjjjjjjjjjjjjjjjjjj
+              hhhhhhhhhhhhhhhhhhhhhhhhhhh
+              kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+              rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+              ffffffffffffffffffffffffffffffffffff
+              nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+              mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+              <kbd>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+                </kbd>ffffffffff
+              ggggggggggggggggggggggggggggggggggggggg
+              ggggggggggggggggggggggggggggggggggggg
+              gggggggggggggggggggggggggggggggggggggg
+              dssssssssssssssssssssssssssss
+              dssssssssssddddddddddddddddddddd
+              ddgggggggggggggggggggggggggggggg
+              hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+              jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+              kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+              yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+              rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+            </p>
+          </span>
+        </div>
+      </div>
   )
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
-import Header from "../Components/Header/Header";
-import Hero from "../Components/Hero/Hero";
-import "./App.css";
-import Residencies from "../Components/Residencies/Residencies";
-import Blog from "../Components/Blog/Blog";
-import Value from "../Components/Value/Value";
-import Contact from "../Components/Contact/Contact";
-import GetStarted from "../Components/GetStarted/GetStarted";
-import Footer from "../Components/Footer/Footer";
+import React from 'react'
 import { Routes, Route } from "react-router-dom";
+import Blog from "../Components/Blog/Blog";
+import Landing from '../Components/Landing/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <div className="white-gradient" />
-        <Header />
-        <Hero />
-      </div>
-      <Routes>
-        <Route path="residencies/:id" element={<Blog />} />
-      </Routes>
-      <Residencies/>
-      <Value />
-      <GetStarted />
-      <Footer />
-    </div>
+    <>
+      <Landing/>
+    <Routes>
+    <Route path="residencies/:id" element={<Blog />} />
+  </Routes>
+      
+    </>
+
+
   );
 }
 
